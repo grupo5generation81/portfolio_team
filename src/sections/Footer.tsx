877e26be4f0d64b4eab52ba656eda49fb935103d
Bowie <larissa.soaress@yahoo.com.br>
2025-06-17 13:59:45 -0300

    Criação do Footer  - closes #7

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,17 @@
+
+function Footer() {
+
+    return (
+        <>
+            <div className="bg-black text-white border-t py-6 w-full px-4 flex flex-col items-center text-center">
+                <p className="mb-2 text-base sm:text-lg">Desenvolvido com muita garra 🤍</p>
+                <p className="mb-2 text-base sm:text-lg">
+                    Em parceria com a <a href="https://brazil.generation.org/" className="font-bold hover:underline">Generation Brasil.</a>
+                </p>
+                <p className="text-sm sm:text-base mt-3">© 2025 Javason's Five. Todos os direitos reservados.</p>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
